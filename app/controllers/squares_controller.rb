7cd2c76ccbcb2814b61ef4d948145ab04969537d
Ani <anisingh@outlook.com>
2023-10-20 17:17:07 +0100

    Fix error

--- app/controllers/squares_controller.rb
+++ app/controllers/squares_controller.rb
@@ -1,18 +1,18 @@
 class SquaresController < ApplicationController
     include SquaresHelper
 
-   
-
     def show
         @square = Square.find(params[:id])
-        if @square.words == ""
+        if @square.words == nil
           @three_words = get_three_words(@square.lat, @square.lng) 
           @three_words = @three_words.split('.').join(' ')
           @square.update(words: @three_words)
+          @image_path = generate_image(@square.words)
+          @square.update(image_url: @image_path)
         end 
-        p "Three Words: #{@square.words}" 
-        @image_path = generate_image(@square.words)
-        # Enqueue the GenerateImageJob to run asynchronously in the background
+        p params[:message] 
+        @message = params[:message]
+
       
     end
     
@@ -21,17 +21,32 @@ class SquaresController < ApplicationController
       @square = Square.find(params[:id])
       remaining_words = get_remaining_words(@square.words)
       p remaining_words
-      @square.update(words: remaining_words)
 
-      if @square.words == remaining_words
-        flash.now[:error] = "Words have not changed."
-        render 'error'
-        
-      elsif remaining_words != ""
-        redirect_to "/squares/#{params[:id]}"
-      else 
+      if remaining_words == ""
         redirect_to "/game"
+      elsif @square.words != remaining_words
+        @square.update(words: remaining_words) 
+        @image_path = generate_image(@square.words)
+        @square.update(image_url: @image_path) 
+        redirect_to "/squares/#{params[:id]}?message=correct"
+      else 
+        redirect_to "/squares/#{params[:id]}?message=incorrect"
       end
+
+
+
+      
+
+      # if @square.words == remaining_words
+      #   # flash.now[:error] = "Words have not changed."
+      #   # render 'error'
+      #   redirect_to "/squares/#{params[:id]}"
+        
+      # elsif remaining_words != ""
+      #   redirect_to "/squares/#{params[:id]}"
+      # else 
+      #   redirect_to "/game"
+      # end
 
  
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_20_094140) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_20_142152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "squares", force: :cascade do |t|
-    t.float "lng"
-    t.float "lat"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "image_url"
-    t.string "words"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
