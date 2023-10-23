9bf0c83f80516b37f8451853a7f89c90c4cf578a
nayan0499 <nayangrg.uni@gmail.com>
2023-10-23 16:33:31 +0100

    link games page to the square page correctly

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,11 @@
 class GameController < ApplicationController
     def index
+        @user = current_user # Fetch the user based on the provided user_id parameter
+        @user_squares = @user.squares.limit(12) 
+        p "here"
+        p"are"
+        p"all"
+        p @user_squares
         render "index"
     end
 end
