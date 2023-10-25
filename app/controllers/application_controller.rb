66582d4cbe8e7b780c738b60ed44e850503a1d40
nayan0499 <nayangrg.uni@gmail.com>
2023-10-25 16:24:06 +0100

    pasing test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
 
         # authenticated_root_path # Redirect to the game page after sign in
-        "/avatars"
+        "/avatar"
 
     end
 
