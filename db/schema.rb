8568bac996e6459707c2f5d5684bb7c6400d7506
Jennifer Wark <128541733+jenny4567@users.noreply.github.com>
2023-10-25 16:56:52 +0100

    square and map page avatars match database

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_122942) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_24_105357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "avatars", force: :cascade do |t|
-    t.string "url"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_avatars_on_user_id"
-  end
 
   create_table "squares", force: :cascade do |t|
     t.float "lng"
@@ -54,11 +46,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_122942) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "avatars", "users"
   add_foreign_key "user_squares", "squares"
   add_foreign_key "user_squares", "users"
 end
