a5d7da144817db137ff9aaa83d3df6ea2afab171
Michaela-VF <fouki.michaela@yahoo.com>
2023-10-25 15:53:43 +0100

    Modified spec files, scheme, avatar profile and avatra controller to pass the failing tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_105357) do
-# ActiveRecord::Schema[7.1].define(version: 2023_10_23_162110) do
-
+ActiveRecord::Schema[7.1].define(version: 2023_10_24_122942) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "avatars", force: :cascade do |t|
+    t.string "url"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_avatars_on_user_id"
+  end
 
   create_table "squares", force: :cascade do |t|
     t.float "lng"
@@ -28,9 +33,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_105357) do
   end
 
   create_table "user_squares", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "square_id"
+    t.string "remaining_words"
+    t.string "image_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_path"
     t.boolean "is_destroyed", default: false
     t.index ["square_id"], name: "index_user_squares_on_square_id"
     t.index ["user_id"], name: "index_user_squares_on_user_id"
@@ -39,15 +47,18 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_105357) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "avatar_file", default: "Alien1.jpeg", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "avatars", "users"
+  add_foreign_key "user_squares", "squares"
+  add_foreign_key "user_squares", "users"
 end
